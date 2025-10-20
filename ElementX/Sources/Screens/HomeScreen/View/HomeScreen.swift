924784676f052c52f3ce66db5ad15afbd8496cc1
shahroz-wilder <shrozali@outlook.com>
2025-10-20 17:22:38 +0500

    fix: auto search close issue on home screen

--- ElementX/Sources/Screens/HomeScreen/View/HomeScreen.swift
+++ ElementX/Sources/Screens/HomeScreen/View/HomeScreen.swift
@@ -19,9 +19,7 @@ struct HomeScreen: View {
     
     @State private var showBackToTop = false
     @State private var hideNavigationBar = false
-    
-    @State private var glassNonce = 0
-    
+
     var body: some View {
         ZStack {
             switch selectedHomeTab {
@@ -48,13 +46,6 @@ struct HomeScreen: View {
         .track(screen: .Home)
         .sentryTrace("\(Self.self)")
         .quickLookPreview($context.mediaPreviewItem)
-        /// This is temp to fix the glass effect not being applied issue
-        /// https://stackoverflow.com/questions/79739688/liquid-glass-not-appearing-for-the-first-launch-of-the-app
-        .id(glassNonce)
-        .onAppear {
-            DispatchQueue.main.async { glassNonce &+= 1 }
-        }
-        /// end
         .overlay(alignment: .top) {
             backToTopButton
                 .ignoresSafeArea(.container, edges: .top)
