5255a2bc6ace3cc53219f056cb35772e8fa3a18b
shahroz-wilder <shrozali@outlook.com>
2025-10-20 21:26:42 +0500

    fix: updated home tabs animation

--- Zero/Sources/Components/SimpleTabButtonsView.swift
+++ Zero/Sources/Components/SimpleTabButtonsView.swift
@@ -89,10 +89,10 @@ struct SimpleFixedTabButtonsView<Tab: Hashable>: View {
                 HorizontalDivider()
             }
         }
-        .offset(y: isShown ? 0 : -20)
+//        .offset(y: isShown ? 0 : -20)
         .opacity(isShown ? 1 : 0)
         .animation(
-            animateView ? .easeOut(duration: 0.25) : .none,
+            animateView ? .easeOut(duration: 0.35) : .none,
             value: isShown
         )
         .task {
