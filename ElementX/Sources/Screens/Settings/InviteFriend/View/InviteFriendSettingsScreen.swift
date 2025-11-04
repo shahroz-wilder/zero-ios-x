fc5ad40cdb6923d78daf99234c671288d582abba
shahroz-wilder <shrozali@outlook.com>
2025-11-04 09:43:31 +0500

    chore: removed unused resources

--- ElementX/Sources/Screens/Settings/InviteFriend/View/InviteFriendSettingsScreen.swift
+++ ElementX/Sources/Screens/Settings/InviteFriend/View/InviteFriendSettingsScreen.swift
@@ -5,78 +5,7 @@ struct InviteFriendSettingsScreen: View {
     @ObservedObject var context: InviteFriendSettingsScreenViewModel.Context
     
     var body: some View {
-        Form {
-            ZeroListRow(kind: .custom {
-                ZStack {
-                    VStack {
-                        Image(asset: Asset.Images.imgInviteFriend)
-                        
-                        Text("Invite a friend.")
-                            .font(.compound.headingMDBold)
-                            .foregroundColor(.compound.textPrimary)
-                            .padding(.top, 24)
-                        
-                        Text("Earn rewards.")
-                            .font(.compound.headingMDBold)
-                            .foregroundColor(.compound.textPrimary)
-                            .padding(.bottom, 24)
-                        
-                        if context.viewState.bindings.messengerInvite.remainingInvites > 0 {
-                            Button {
-                                UIPasteboard.general.string = inviteCodeMessage(inviteSlug: context.viewState.bindings.messengerInvite.slug)
-                                context.send(viewAction: .inviteCopied)
-                            } label: {
-                                Image(asset: Asset.Images.btnCopyInviteCode)
-                            }
-                            .padding(.vertical, 16)
-                            
-                            Text("\(context.viewState.bindings.messengerInvite.remainingInvites) Remaining")
-                                .font(.zero.bodyLG)
-                                .foregroundColor(.compound.textSecondary)
-                                .padding(.bottom, 16)
-                        } else {
-                            Text("Thank you! You’ve used all of your available invites. We’ll let you know when you can invite more people.")
-                                .font(.zero.bodyLG)
-                                .foregroundColor(.compound.textSecondary)
-                                .padding(.vertical, 16)
-                                .padding(.horizontal, 24)
-                        }
-                    }
-                    
-                    if context.viewState.bindings.inviteCopied {
-                        inviteCopiedView
-                    }
-                }
-            })
-        }
-        .zeroList()
-    }
-    
-    var inviteCopiedView: some View {
-        VStack {
-            Image(asset: Asset.Images.checkIcon)
-                .resizable()
-                .frame(width: 48, height: 48)
-            
-            Text("Invite Copied")
-                .font(.compound.bodyLGSemibold)
-                .foregroundStyle(.compound.textPrimary)
-        }
-        .padding(.all, 24)
-        .background(
-            RoundedRectangle(cornerRadius: 12)
-                .fill(.compound.bgCanvasDefaultLevel1)
-        )
-    }
-    
-    private func inviteCodeMessage(inviteSlug: String) -> String {
-        """
-        Here's your invite code to ZERO Messenger:
-        \(inviteSlug)
-
-        Join early, earn more:
-        https://zos.zero.tech/get-access
-        """
+        EmptyView()
     }
 }
 
---
+++ Zero/Sources/Screens/Authentication/ZeroAuthenticationMethod.swift
@@ -0,0 +1,11 @@
+//
+// Copyright 2025 New Vector Ltd.
+//
+// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+// Please see LICENSE files in the repository root for full details.
+//
+
+public enum ZeroAuthenticationMethod: String, Equatable, CaseIterable {
+    case web3 = "Web3"
+    case email = "Email"
+}
